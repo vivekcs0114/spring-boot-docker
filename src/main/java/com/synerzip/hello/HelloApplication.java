1273665dd9eb3ebd61872c416d19d26e5bace6e7
Vivek Tiwari <vivekcs0114@gmail.com>
2018-03-21 23:56:11 +0530

    spring boot docker image

---
+++ src/main/java/com/synerzip/hello/HelloApplication.java
@@ -0,0 +1,21 @@
+package com.synerzip.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	@GetMapping()
+	public String sayHello() {
+		return "Hello World!!!";
+	}
+}
+
